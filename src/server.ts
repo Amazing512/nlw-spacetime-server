5eac46cbdbfdf59a8d864407408ef1ba1762f39e
Matheus Mastelini <matheusmastelini@gmail.com>
2023-05-19 00:04:13 -0300

    update: Image Static upload

--- src/server.ts
+++ src/server.ts
@@ -3,21 +3,31 @@ import 'dotenv/config'
 import fastify from 'fastify'
 import cors from '@fastify/cors'
 import jwt from '@fastify/jwt'
+import multipart from '@fastify/multipart'
 import { memoriesRoutes } from './routes/memories'
 import { authRoutes } from './routes/auth'
+import { uploadRoutes } from './routes/upload'
+import { resolve } from 'node:path'
 
 const app = fastify()
 
+app.register(multipart)
+
 app.register(cors, {
   origin: true, // todas as urls poderao acessar o back-end
+})
+app.register(require('@fastify/static'), {
+  root: resolve(__dirname, '../uploads'),
+  prefix: '/uploads',
 })
 
 app.register(jwt, {
   secret: 'spacetime',
 })
 
-app.register(memoriesRoutes)
+app.register(uploadRoutes)
 app.register(authRoutes)
+app.register(memoriesRoutes)
 
 app
   .listen({
